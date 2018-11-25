d9b3f5ca114eb225d7b121e451fe49870d8708d2
schmidt-aaron <aschmidt808@gmail.com>
2018-11-25 12:51:32 -0800

    intial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+
+
+export default () => (
+  <main>
+    <Header
+      headerText="Light my Fire"
+       />
+    <Nav></Nav>
+    <div>Hello world!</div>
+
+  </main>
+)

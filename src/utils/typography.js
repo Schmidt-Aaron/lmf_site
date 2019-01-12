7b47e7da2a5abe5fc4e424f72009c9abffa0c182
schmidt-aaron <aschmidt808@gmail.com>
2019-01-12 13:34:10 -0800

    adjust header font weight + clean up typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,9 @@
 import Typography from "typography";
 import funstonTheme from "typography-theme-funston";
-import githubTheme from "typography-theme-github";
 
 // funston theme modifications
 funstonTheme.baseLineHeight = "1.625";
+funstonTheme.headerWeight = "500";
 funstonTheme.headerFontFamily = [
   "Roboto",
   "-apple-system",
@@ -35,117 +35,7 @@ funstonTheme.googleFonts = [
   }
 ];
 
-// // funston theme overrides
-// funstonTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-//   {/* prettier-ignore */}
-//   'headerFontFamily': [
-//     "Roboto",
-//     "-apple-system",
-//     "BlinkMacSystemFont",
-//     "Segoe UI",
-//     "Helvetica",
-//     "Arial",
-//     "sans-serif",
-//     "Apple Color Emoji",
-//     "Segoe UI Emoji",
-//     "Segoe UI Symbol"
-//   ],
-//   {/* prettier-ignore */}
-//   'bodyFontFamily': [
-//     "-apple-system",
-//     "BlinkMacSystemFont",
-//     "Segoe UI",
-//     "Roboto",
-//     "Helvetica",
-//     "Arial",
-//     "sans-serif",
-//     "Apple Color Emoji",
-//     "Segoe UI Emoji",
-//     "Segoe UI Symbol"
-//   ]
-// });
-
-// const typography = new Typography(githubTheme);
 const typography = new Typography(funstonTheme);
-// const typography = new Typography(judahTheme);
-// const typography = new Typography(grandViewTheme);
-
-// const typography = new Typography({
-//   baseFontSize: "20px",
-//   baseLineHeight: 1.625,
-//   headerFontFamily: [
-//     "Roboto",
-//     "-apple-system",
-//     "BlinkMacSystemFont",
-//     "Segoe UI",
-//     "Helvetica",
-//     "Arial",
-//     "sans-serif",
-//     "Apple Color Emoji",
-//     "Segoe UI Emoji",
-//     "Segoe UI Symbol"
-//   ],
-//   bodyFontFamily: [
-//     "-apple-system",
-//     "BlinkMacSystemFont",
-//     "Segoe UI",
-//     "Roboto",
-//     "Helvetica",
-//     "Arial",
-//     "sans-serif",
-//     "Apple Color Emoji",
-//     "Segoe UI Emoji",
-//     "Segoe UI Symbol"
-//   ],
-//   scaleRatio: 2,
-//   bodyColor: "hsla(0,0%,0%,0.8)",
-//   headerWeight: 600,
-//   bodyWeight: "normal",
-//   boldWeight: 600,
-//   // Github has all block elements use 1/2 rhythm not a full rhythm.
-//   blockMarginBottom: 1 / 2,
-//   overrideStyles: ({ rhythm }) => ({
-//     h1: {
-//       paddingBottom: `calc(${rhythm(1 / 4)} - 1px)`,
-//       marginBottom: rhythm(3 / 4),
-//       marginTop: rhythm(1.5)
-//     },
-//     h2: {
-//       paddingBottom: `calc(${rhythm(1 / 4)} - 1px)`,
-//       marginBottom: rhythm(1 / 4),
-//       marginTop: rhythm(1)
-//     },
-//     h6: {
-//       color: gray(47)
-//     },
-//     "h3,h4,h5,h6": {
-//       marginBottom: rhythm(1 / 2),
-//       marginTop: rhythm(1)
-//     },
-//     "ol,ul": {
-//       marginLeft: rhythm(1.25)
-//     },
-//     // children ol, ul
-//     "li>ol,li>ul": {
-//       marginLeft: rhythm(1.25)
-//     },
-//     a: {
-//       color: "#4078c0",
-//       textDecoration: "none"
-//     },
-//     "a:hover,a:active": {
-//       textDecoration: "underline"
-//     },
-//     blockquote: {
-//       borderLeft: `4px solid ${gray(87)}`,
-//       color: gray(47),
-//       marginTop: 0,
-//       marginRight: 0,
-//       marginLeft: 0,
-//       paddingLeft: `calc(${rhythm(1 / 2)} - 1px)`
-//     }
-//   })
-// });
 
 export default typography;
 export const rhythm = typography.rhythm;

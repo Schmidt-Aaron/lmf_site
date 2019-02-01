3586bfefe485a8426de9701beead512098557589
schmidt-aaron <aschmidt808@gmail.com>
2019-02-01 12:47:46 -0800

    contact form style updates

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,15 +4,29 @@ import { css, jsx } from "@emotion/core";
 import styled from "@emotion/styled";
 
 const Label = styled.label`
-  display: inline-block;
+  display: none;
   min-width: 110px;
   text-align: right;
   vertical-align: top;
   padding-right: 10px;
   margin-bottom: 20px;
+  @media (max-width: 1024px) {
+    display: none;
+    min-width: 100%;
+    text-align: left;
+    margin-bottom: 10px;
+  }
 `;
 const Input = styled.input`
-  width: 300px;
+  width: calc(100% - 140px);
+  padding: 5px;
+  margin-bottom: 1rem;
+  @media (max-width: 1024px) {
+    display: block;
+    width: 100%;
+    text-align: left;
+    border: 0.5px solid hsla(0, 0%, 0%, 0.33);
+  }
 `;
 
 const ContactForm = () => {
@@ -24,26 +38,51 @@ const ContactForm = () => {
       action="/thanks/index.html"
       netlify-honeypot="bot-field"
     >
-      <fieldset style={{ border: `0 solid black` }}>
+      <fieldset
+        css={css`
+          border: 0 solid black;
+          padding: 5px;
+        `}
+      >
         <input hidden className="contact" name="bot-field" />
-        <Label htmlfor="name">Full Name:</Label>
+        <Label htmlfor="name">Name:</Label>
         <Input type="text" name="name" placeholder="Name" />
-        <br />
+
         <Label htmlfor="email">Email:</Label>
         <Input type="email" name="email" placeholder="Email" />
-        <br />
+
         <Label htmlfor="message">Message: </Label>
         <textarea
           name="message"
           placeholder="How can we help?"
           rows="5"
-          style={{ width: `calc(100% - 140px)` }}
+          css={css`
+            width: calc(100% - 140px);
+            min-width: 300px;
+            padding: 5px;
+            margin-bottom: 1rem;
+            @media (max-width: 1024px) {
+              display: block;
+              width: 100%;
+              margin-left: 0;
+              text-align: left;
+              border: 0.5px solid hsla(0, 0%, 0%, 0.33);
+            }
+          `}
         />
-        <p>
-          <button type="submit" style={{ marginLeft: `110px` }}>
-            Submit
-          </button>
-        </p>
+
+        <button
+          type="submit"
+          css={css`
+            margin-left: 110px;
+            @media (max-width: 1024px) {
+              display: block;
+              margin-left: 0;
+            }
+          `}
+        >
+          Submit
+        </button>
       </fieldset>
     </form>
   );

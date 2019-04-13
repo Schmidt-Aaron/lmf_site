330264e100137cfbfdab9aa4ced49592e3e476bd
schmidt-aaron <aschmidt808@gmail.com>
2019-04-13 10:35:32 -0700

    fix typos

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -24,14 +24,14 @@ const Mission = props => {
           }
         `}
       >
-        In the Seattle area alone, there are around 400 home fires every year.
-        Immediately following these tragedies, many families are left homeless
-        with no place to go. Light My Fire helps local fire departments by
+        In the Seattle-area alone, there are around 400 home fires every year.
+        Immediately following these tragedies many families are left homeless
+        with no place to go. Light My Fire (LMF) helps local fire departments by
         gathering donations. These donations are then used by local fire
         fighters to help families find temporary housing, food, water, and other
         essentials immediately following a home fire. Established in 2014, LMF
-        (Light My Fire) of Puget Sound was organized in an effort to help
-        families affected by fire damage in the western Washington area.
+        of Puget Sound was organized in an effort to help families affected by
+        fire damage in the western Washington area.
       </p>
     </section>
   );
